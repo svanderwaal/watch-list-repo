dda52d5dca8050ac3fbad8cc1f01e1a1d2650156
Sarah van der Waal <svanderwaal@hotmail.com>
2022-02-25 09:54:08 +1100

    updated html.erb files

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,6 +22,12 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def set_list
